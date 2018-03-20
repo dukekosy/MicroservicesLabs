bd03f843d015266ac475efc0048c4243d1cf1387
dukekosy <dukekosy@gmail.com>
2018-03-19 19:20:25 -0700

    lab4

---
+++ lab4/lab-4-article/src/main/java/com/monkey/lab4article/Lab4ArticleApplication.java
@@ -0,0 +1,14 @@
+package com.monkey.lab4article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Lab4ArticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4ArticleApplication.class, args);
+	}
+}
